df081a5875542bfcb5ebab0de372180d964ac26e
kstaff123 <47585247+kstaff123@users.noreply.github.com>
2025-02-03 20:47:24 -0800

    Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,19 @@ import { useState } from "react";
 
 export function Header(){
     return(
-        <header class="bg-gray-800 text-white w-full h-16 flex items-center justify-start px-4 ">
-            <h1 class="text-center">ClassMate</h1>
+        <header class="bg-white text-white w-full h-16 grid grid-cols-3 px-4 items-center">
+            <h1 class="text-center font-medium text-black justify-self-start">ClassMate</h1>
+            <div class=" text-xl flex items-center space-x-4 justify-center">
+                <div class="bg-[#173E82] w-fit h-fit p-2 rounded-4xl">
+                    <h1 class="text-white">Dashboard</h1>
+                </div>
+                <h1 class="text-black font-medium">Class Browser</h1>
+            </div>
+            <div class="text-black text-sm font-medium ml-4 justify-self-end">
+                <h2 class="">login</h2>
+                </div>
+            
+            
         </header>
     );
 }
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ import { Header } from "../components/Header";
 
 export function Homepage() {
     return(
-        <main class="font-primary font-bold text-2xl">
+        <main class="font-primary font-bold text-2xl bg-gray-100 h-screen">
             <div class="flex">
                 <Header/>
             </div>
