013607eaeb4211a36fd90a0d98c5d075dbde8c64
kstaff123 <47585247+kstaff123@users.noreply.github.com>
2025-02-03 22:07:56 -0800

    Added text to grid sections

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -8,9 +8,17 @@ export function Homepage() {
             <div class="flex-none">
                 <Header/>
             </div>
-            <div class="grid grid-cols-3 items-center grid-flow-col flex-grow p-8 gap-x-8 " >
-                <div class="bg-white col-span-2 w-full h-7/8 rounded-4xl shadow-xl"></div>
-                <div class="bg-white col-span-1 w-full h-7/8 rounded-4xl shadow-xl"></div>
+            <div class="grid grid-cols-3 items-center grid-flow-col flex-grow p-8 gap-x-8" >
+                <div class="bg-white col-span-2 w-full h-7/8 rounded-4xl shadow-xl">
+                    <div class="flex items-center p-4">
+                        <h2 class="text-xl font-medium">Schedule</h2>
+                    </div>
+                </div>
+                <div class="bg-white col-span-1 w-full h-7/8 rounded-4xl shadow-xl">
+                    <div class="flex items-center p-4">
+                        <h2 class="text-xl font-medium">Class Cart</h2>
+                    </div>
+                </div>
             </div>
         </main>
     );
