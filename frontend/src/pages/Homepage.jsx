e54202c32f12859d646ba7d207c312dc30e5c9d6
kstaff123 <47585247+kstaff123@users.noreply.github.com>
2025-02-27 12:31:11 -0800

    fixed class cart persistence, fixed none options

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,30 +2,35 @@ import "../App.css";
 import { useState } from "react";
 import { Header } from "../components/Header";
 import { Schedule } from "../components/Schedule";
+import { Cart } from "../components/Cart";
 
 export function Homepage() {
-    return(
-        <main class="font-primary font-bold text-2xl bg-gray-100 h-screen overflow-hidden flex-col flex">
-            <div class="flex-none">
-                <Header/>
+  return (
+    <main class="font-primary font-bold text-2xl bg-gray-100 h-screen overflow-hidden flex-col flex">
+      <div class="flex-none">
+        <Header />
+      </div>
+      <div class="grid grid-cols-3 items-center grid-flow-col flex-grow px-8 gap-x-8 ">
+        <div class="bg-white col-span-2 w-full h-[87.5vh] max-h-7/8 rounded-4xl shadow-xl">
+          <div class="flex flex-col p-4 max-h-full">
+            <h2 class="text-2xl font-medium">Schedule</h2>
+            <div
+              id="scrollable-container"
+              className="mx-.9 overflow-y-scroll overflow-x-hidden"
+            >
+              <Schedule />
             </div>
-            <div class="grid grid-cols-3 items-center grid-flow-col flex-grow px-8 gap-x-8 " >
-                <div class="bg-white col-span-2 w-full h-[87.5vh] max-h-7/8 rounded-4xl shadow-xl">
-                    <div class="flex flex-col p-4 max-h-full">
-                        <h2 class="text-2xl font-medium">Schedule</h2>
-                        <div id="scrollable-container" className="mx-.9 overflow-y-scroll overflow-x-hidden">
-                            <Schedule/>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-                <div class="bg-white col-span-1 w-full h-[87.5vh] max-h-7/8 rounded-4xl shadow-xl">
-                    <div class="flex items-center p-4">
-                        <h2 class="text-2xl font-medium">Class Cart</h2>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div class="bg-white col-span-1 w-full h-[87.5vh] max-h-7/8 rounded-4xl shadow-xl">
+          <div className="flex p-4 flex-col">
+            <h2 className="text-2xl font-medium">Class Cart</h2>
+            <div className="">
+              <Cart />
             </div>
-        </main>
-    );
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
